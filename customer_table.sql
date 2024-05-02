cc0ab87d48d505db989c7b973a6cab184fa8ff16
Daniel <mwakamoledaniel@gmail.com>
2024-05-02 17:25:08 +0300

    Customer Table

---
+++ customer_table.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 02, 2024 at 04:13 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `practice`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer table`
+--
+
+CREATE TABLE `customer table` (
+  `CustomerID` int(20) NOT NULL,
+  `Customer Name` text NOT NULL,
+  `Contact Name` text NOT NULL,
+  `Address` varchar(30) NOT NULL,
+  `City` varchar(30) NOT NULL,
+  `PostalCode` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer table`
+--
+ALTER TABLE `customer table`
+  ADD PRIMARY KEY (`CustomerID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
